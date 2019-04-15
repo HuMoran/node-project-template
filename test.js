4f1cd776797e48407aa578da5f6afc55c28ccd42
Tao.Hu <htax2013@gmail.com>
2019-04-15 14:42:42 +0800

    feat: add test.js

---
+++ test.js
@@ -0,0 +1,11 @@
+/*
+ * File: \test.js
+ * Project: node-project-template
+ * Description: 
+ * Created By: Tao.Hu 2019-04-15
+ * -----
+ * Last Modified: 2019-04-15 2:38:50
+ * Modified By: Tao.Hu
+ * -----
+ * Copyright (c) 2019 KiwiSec Tech Co.,Ltd
+ */
